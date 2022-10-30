d8232bc391ea8c9cafed4b8bcf645a22d9713615
LevKharl <337029@niuitmo.ru>
2022-10-30 14:28:22 +0100

    deleted an unused func.

--- solution/include/image.h
+++ solution/include/image.h
@@ -17,6 +17,4 @@ struct pixel
   uint8_t b, g, r;
 };
 
-struct pixel *get_pixel(struct image *img, uint64_t col, uint64_t row);
-
 #endif
--- solution/src/image.c
+++ solution/src/image.c
@@ -18,8 +18,3 @@ void image_destroy(struct image *img)
         free(img->data);
     }
 }
-
-struct pixel *get_pixel(struct image *img, uint64_t col, uint64_t row)
-{
-    return &img->data[col + row * img->width];
-}
